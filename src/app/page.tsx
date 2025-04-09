9d56413173de68d5d8a02e6fbacf9dc2c5a3c59e
roni505 <161919194+roni505@users.noreply.github.com>
2025-04-09 23:52:50 +0530

    prittier added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,22 @@
 export default function Home() {
   return (
     <>
-
-
-
-
-      <header>
-        Here will be the hero
-      </header>
+      <header>Here will be the hero</header>
 
       <main>
-        
-        <section className="mt-52">
+        <section className="rounded bg-blue-500 p-2 text-white hover:bg-blue-600">
           Features
         </section>
-        <section className="mt-52">
-          Customers
-        </section>
-        <section className="mt-52">
-          Benefits
-        </section>
-        <section className="mt-52">
-          Pricing
-        </section>
+        <section className="mt-52">Customers</section>
+        <section className="mt-52">Benefits</section>
+        <section className="mt-52">Pricing</section>
+        <section className="mt-52">Pricing</section>
+        <section className="mt-52">Pricing</section>
+        <section className="mt-52">Pricing</section>
+        <section className="mt-52">Pricing</section>
       </main>
 
-      <footer>
-        Footer
-      </footer>
+      <footer>Footer</footer>
     </>
   );
 }
